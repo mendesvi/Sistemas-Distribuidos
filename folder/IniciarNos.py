2cbc7f245964fc15dbd6f44d43575578341a3734
Victor Mendes <82238939+mendesvi@users.noreply.github.com>
2023-06-24 16:17:54 -0300

    Add files via upload

---
+++ folder/IniciarNos.py
@@ -0,0 +1,22 @@
+import subprocess
+
+subprocess.Popen('python Atividade4.py A',
+                 creationflags=subprocess.CREATE_NEW_CONSOLE)
+subprocess.Popen('python Atividade4.py B',
+                 creationflags=subprocess.CREATE_NEW_CONSOLE)
+subprocess.Popen('python Atividade4.py C',
+                 creationflags=subprocess.CREATE_NEW_CONSOLE)
+subprocess.Popen('python Atividade4.py D',
+                 creationflags=subprocess.CREATE_NEW_CONSOLE)
+subprocess.Popen('python Atividade4.py E',
+                 creationflags=subprocess.CREATE_NEW_CONSOLE)
+subprocess.Popen('python Atividade4.py F',
+                 creationflags=subprocess.CREATE_NEW_CONSOLE)
+subprocess.Popen('python Atividade4.py G',
+                 creationflags=subprocess.CREATE_NEW_CONSOLE)
+subprocess.Popen('python Atividade4.py H',
+                 creationflags=subprocess.CREATE_NEW_CONSOLE)
+subprocess.Popen('python Atividade4.py I',
+                 creationflags=subprocess.CREATE_NEW_CONSOLE)
+subprocess.Popen('python Atividade4.py J',
+                 creationflags=subprocess.CREATE_NEW_CONSOLE)
